20dd0fe9657dbb41f731a6df038f0669a8cefb58
Tan Moy <tanmoy.pto@gmail.com>
2017-05-11 14:05:03 +0530

    styled add page

--- src/components/add.js
+++ src/components/add.js
@@ -16,64 +16,76 @@ class Add extends Component {
       return <Redirect to="/" />;
     }
     return (
-      <Form
-        onSubmit={values => {
-          values.SSN = `${Math.ceil(1000 * Math.random())}-${Math.ceil(1000 * Math.random())}-${Math.ceil(100 * Math.random())}`;
-          this.props.createData(values);
-          this.setState({}); //comment to stop auto redirection
-          console.log("Success!", values);
-        }}
-        validate={values => {
-          const {
-            firstName,
-            lastName,
-            email,
-            phoneNumber,
-            cityName,
-            status
-          } = values;
-          return {
-            firstName: !firstName ? "firstName is required" : undefined,
-            lastName: !lastName ? "lastName is required" : undefined,
-            email: !email ? "emailId is required" : undefined,
-            phoneNumber: !phoneNumber ? "Phone number is required" : undefined,
-            cityName: !cityName ? "City Name is required" : undefined,
-            status: !status ? "status is required" : undefined
-          };
-        }}
-      >
-        {({ submitForm, setAllValues }) => {
-          return (
-            <form onSubmit={submitForm}>
-              <div>
-                <Text field="firstName" placeholder="First Name" />
-                <Text field="lastName" placeholder="Last Name" />
-                <Text field="email" placeholder="Email" />
-                <Text field="phoneNumber" placeholder="Phone" />
-                <Text field="cityName" placeholder="City" />
-                <Text field="status" placeholder="Status" />
-                <button type="submit">Submit</button>
-              </div>
-              <div>
-                <button
-                  onClick={() =>
-                    setAllValues({
-                      firstName: "",
-                      lastName: "",
-                      email: "",
-                      phoneNumber: "",
-                      cityName: "",
-                      status: "",
-                      SSN: ""
-                    })}
-                >
-                  Clear
-                </button>
-              </div>
-            </form>
-          );
-        }}
-      </Form>
+      <div className="addPage">
+        <Form
+          onSubmit={values => {
+            values.SSN = `${Math.ceil(1000 * Math.random())}-${Math.ceil(1000 * Math.random())}-${Math.ceil(100 * Math.random())}`;
+            this.props.createData(values);
+            this.setState({}); //comment to stop auto redirection
+            console.log("Success!", values);
+          }}
+          validate={values => {
+            const {
+              firstName,
+              lastName,
+              email,
+              phoneNumber,
+              cityName,
+              status
+            } = values;
+            return {
+              firstName: !firstName ? "firstName is required" : undefined,
+              lastName: !lastName ? "lastName is required" : undefined,
+              email: !email ? "emailId is required" : undefined,
+              phoneNumber: !phoneNumber
+                ? "Phone number is required"
+                : undefined,
+              cityName: !cityName ? "City Name is required" : undefined,
+              status: !status ? "status is required" : undefined
+            };
+          }}
+        >
+          {({ submitForm, setAllValues }) => {
+            return (
+              <form onSubmit={submitForm}>
+                <div>
+                  <span>First Name</span>
+                  <Text field="firstName" placeholder="First Name" />
+                  <span>Last Name</span>
+                  <Text field="lastName" placeholder="Last Name" />
+                  <span>Email</span>
+                  <Text field="email" placeholder="Email" />
+                  <span>Phone</span>
+                  <Text field="phoneNumber" placeholder="Phone" />
+                  <span>City</span>
+                  <Text field="cityName" placeholder="City" />
+                  <span>Status</span>
+                  <Text field="status" placeholder="Status" />
+                  <div className="inlinebuttons">
+                    <button
+                      id="clear"
+                      onClick={() =>
+                        setAllValues({
+                          firstName: "",
+                          lastName: "",
+                          email: "",
+                          phoneNumber: "",
+                          cityName: "",
+                          status: "",
+                          SSN: ""
+                        })}
+                    >
+                      Clear
+                    </button>
+                    <button id="submit" type="submit">Submit</button>
+                  </div>
+                </div>
+
+              </form>
+            );
+          }}
+        </Form>
+      </div>
     );
   }
 }
