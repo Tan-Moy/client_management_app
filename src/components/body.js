6837c9bb74622074aeb4441e60d009aa79c04d71
Tan Moy <tanmoy.pto@gmail.com>
2017-05-11 15:58:53 +0530

    bugfixes

--- src/components/body.js
+++ src/components/body.js
@@ -1,6 +1,3 @@
-//<Route path="/roster" component={Roster} />
-//<Route path="/schedule" component={Schedule} />
-
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import Tiles from "./tiles";
@@ -9,13 +6,15 @@ import Add from "./add";
 import Edit from "./edit";
 import View from "./view";
 
-const Body = () => (
-  <div>
-    <Route exact path="/" component={Tables} />
-    <Route path="/user/add" component={Add} />
-    <Route path="/user/edit/:id" component={Edit} />
-    <Route path="/user/view/:id" component={View} />
-  </div>
-);
-
-export default Body;
+export default class Body extends Component {
+  render() {
+    return (
+      <div>
+        <Route path="/" component={Tables} />
+        <Route path="/user/add" component={Add} />
+        <Route path="/user/edit/:id" component={Edit} />
+        <Route path="/user/view/:id" component={View} />
+      </div>
+    );
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,50 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const WriteFilePlugin = require("write-file-webpack-plugin");
 
 module.exports = {
-    entry: "./src/root.js",
-    output: {
-        path: __dirname + "/dist",
-        filename: "bundle.js",
-        publicPath: "/"
-    },
+  entry: "./src/root.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js"
+    //publicPath: "/dist"
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: ["css-loader", "sass-loader"]
-                })
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: "babel-loader"
-            },
-            {
-                test: /\.json$/,
-                use: "json-loader"
-            }
-        ]
-    },
-
-    devServer: {
-        contentBase: __dirname + "/dist",
-        historyApiFallback: true,
-        compress: true,
-        stats: "errors-only",
-        open: true
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "Project",
-            template: "./src/root.ejs"
-        }),
-        new ExtractTextPlugin("app.css")
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader", "sass-loader"]
+        })
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.json$/,
+        use: "json-loader"
+      }
     ]
+  },
+
+  devServer: {
+    contentBase: __dirname + "/dist",
+    historyApiFallback: true,
+    compress: true,
+    stats: "errors-only",
+    open: true
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Project",
+      template: "./src/root.ejs"
+    }),
+    new ExtractTextPlugin("app.css"),
+    new WriteFilePlugin()
+  ]
 };
